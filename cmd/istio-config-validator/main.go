5c41ba4bdcbd5edb78adc35dbf66b0dd1d6310d9
Andreas Jaggi <andreas.jaggi@getyourguide.com>
2020-05-29 12:12:07 +0200

    Also accept config/test folders

--- cmd/istio-config-validator/main.go
+++ cmd/istio-config-validator/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -20,32 +21,46 @@ func (m *multiValueFlag) Set(value string) error {
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s -t <testcases1.yml> [-t <testcases2.yml> ...] <istioconfig1.yml> [<istioconfig2.yml> ...]\n\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s -t <testcases1.yml|testcasesdir1> [-t <testcases2.yml|testcasesdir2> ...] <istioconfig1.yml|istioconfigdir1> [<istioconfig2.yml|istioconfigdir2> ...]\n\n", os.Args[0])
 		flag.PrintDefaults()
 	}
-	var testCaseFiles multiValueFlag
-	flag.Var(&testCaseFiles, "t", "Testcase files")
+	var testCaseParams multiValueFlag
+	flag.Var(&testCaseParams, "t", "Testcase files/folders")
 	flag.Parse()
-	istioConfigFiles := flag.Args()
+	istioConfigFiles := getFiles(flag.Args())
+	testCaseFiles := getFiles(testCaseParams)
 
 	if len(testCaseFiles) < 1 {
-		fmt.Fprintf(os.Stderr, "Missing testcases file, please provide at least one testcases file\n")
+		fmt.Fprintf(os.Stderr, "Missing testcases file/folder, please provide at least one testcases file or folder\n")
 		flag.Usage()
 		os.Exit(1)
 	}
 	if len(istioConfigFiles) < 1 {
-		fmt.Fprintf(os.Stderr, "Missing istio config file, please provide at least one istio config file\n")
+		fmt.Fprintf(os.Stderr, "Missing istio config file/folder, please provide at least one istio config file or folder\n")
 		flag.Usage()
 		os.Exit(1)
 	}
 
 	// TODO: instead of printing out the file names, call the parsing&validation functions
-	fmt.Println("Received the following test case files:")
+	fmt.Println("Received the following test case files/folders:")
 	for _, file := range testCaseFiles {
 		fmt.Println(file)
 	}
-	fmt.Println("Received the following istio config files:")
+	fmt.Println("Received the following istio config files/folders:")
 	for _, file := range istioConfigFiles {
 		fmt.Println(file)
 	}
+}
+
+func getFiles(names []string) []string {
+	var files []string
+	for _, name := range names {
+		filepath.Walk(name, func(path string, info os.FileInfo, err error) error {
+			if !info.IsDir() {
+				files = append(files, path)
+			}
+			return nil
+		})
+	}
+	return files
 }
