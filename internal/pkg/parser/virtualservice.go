727b52ac533e419e28a164b81077e41182d809e9
Fernando Cainelli <fernando.cainelli@getyourguide.com>
2020-05-30 09:55:10 +0200

    skip files not with the right type

--- internal/pkg/parser/virtualservice.go
+++ internal/pkg/parser/virtualservice.go
@@ -5,8 +5,10 @@ import (
 	"io/ioutil"
 
 	"github.com/ghodss/yaml"
+	"go.uber.org/zap/zapcore"
 
 	v1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	"istio.io/pkg/log"
 )
 
 func parseVirtualServices(files []string) ([]*v1alpha3.VirtualService, error) {
@@ -21,16 +23,19 @@ func parseVirtualServices(files []string) ([]*v1alpha3.VirtualService, error) {
 		// we need to transform yaml to json so the marsheler from istio works
 		jsonBytes, err := yaml.YAMLToJSON(fileContet)
 		if err != nil {
-			return []*v1alpha3.VirtualService{}, err
+			log.Debug("error converting yaml to json", zapcore.Field{Key: "file", String: file})
+			continue
 		}
 
 		virtualService := &v1alpha3.VirtualService{}
 		err = json.Unmarshal(jsonBytes, virtualService)
 		if err != nil {
-			return []*v1alpha3.VirtualService{}, err
+			log.Debug("error while trying to unmarshal virtualservice", zapcore.Field{Key: "file", String: file})
+			continue
 		}
 
-		if virtualService.Name == "" {
+		if virtualService.Kind != "VirtualService" {
+			log.Debug("file is not Kind VirtualService", zapcore.Field{Key: "file", String: file})
 			continue
 		}
 
